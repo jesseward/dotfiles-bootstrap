f4b3e3867ce18f3d82a9898289a9e90fd3990d44
jesseward <jesse.ward@gmail.com>
2013-10-13 10:12:00 -0400

    adding vim-gitgutter

--- init/vim.sh
+++ init/vim.sh
@@ -5,7 +5,8 @@ AUTOLOAD_DIR="${HOME}/.vim/autoload"
 # vim plugins to install
 PACKAGES="https://github.com/bling/vim-airline 
 https://github.com/tsaleh/vim-matchit
-https://github.com/altercation/vim-colors-solarized"
+https://github.com/altercation/vim-colors-solarized
+https://github.com/airblade/vim-gitgutter.git"
 
 # package list
 centos_packages() {
